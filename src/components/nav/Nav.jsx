64f2b513750d645f4eaf1be79b7a5d5610c62101
The24thDS <davidsima@windowslive.com>
2021-04-08 17:27:53 +0300

    Refactored nav links

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -33,52 +33,46 @@ const Nav = ({
     history.push('/login');
   };
 
-  const renderBrandLink = () =>
-    showBrand === true ? <BrandLink to="/">KAREN</BrandLink> : '';
-  const renderAllModelsLink = () => <NavLink to="/models">All Models</NavLink>;
-  const renderModelUploadLink = () =>
-    isLoggedIn === true ? <NavLink to="/models/new">Upload</NavLink> : '';
-  const renderSearchInput = () =>
-    showSearch === true ? (
-      <SearchInput
-        classNames={{
-          container: tw`z-10 relative text-sm my-2 w-100 ${searchInputContainer}`,
-          input: 'px-4',
-          button: 'p-2',
-        }}
-      />
-    ) : (
-      ''
-    );
-  const renderLogoutLink = () =>
-    isLoggedIn === true ? (
-      <NavLink onClick={logUserOut} to="/login">
-        Logout
-      </NavLink>
-    ) : (
-      ''
-    );
+  const brandLink = showBrand && <BrandLink to="/">KAREN</BrandLink>;
+  const allModelsLink = <NavLink to="/models">All Models</NavLink>;
+  const modelUploadLink = isLoggedIn && (
+    <NavLink to="/models/new">Upload</NavLink>
+  );
+  const searchInput = showSearch && (
+    <SearchInput
+      classNames={{
+        container: tw`z-10 relative text-sm my-2 w-100 ${searchInputContainer}`,
+        input: 'px-4',
+        button: 'p-2',
+      }}
+    />
+  );
+  const logoutLink = isLoggedIn && (
+    <NavLink onClick={logUserOut} to="/login">
+      Logout
+    </NavLink>
+  );
   const userGreetingItem = isLoggedIn && (
     <NavItem>Hello, {currentUserData.email.split('@')[0]}</NavItem>
   );
-  const renderRegisterLink = () =>
-    isLoggedIn === false ? <NavLink to="/register">Register</NavLink> : '';
-  const renderLoginLink = () =>
-    isLoggedIn === false ? <NavLink to="/login">Login</NavLink> : '';
+  const registerLink = !isLoggedIn && (
+    <NavLink to="/register">Register</NavLink>
+  );
+  const loginLink = !isLoggedIn && <NavLink to="/login">Login</NavLink>;
 
   return (
     <nav className="flex mx-auto justify-between px-14 h-14 bg-gray-900">
       <ul className="flex">
-        {renderBrandLink()}
-        {renderAllModelsLink()}
-        {renderModelUploadLink()}
+        {brandLink}
+        {allModelsLink}
+        {modelUploadLink}
       </ul>
-      {renderSearchInput()}
+      {searchInput}
       <ul className="flex">
-        {renderLoginLink()}
-        {renderRegisterLink()}
+        {loginLink}
+        {registerLink}
         {userGreetingItem}
-        {renderLogoutLink()}
+        {logoutLink}
       </ul>
     </nav>
   );
