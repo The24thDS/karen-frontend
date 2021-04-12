79b4b0e6bd82f192853d0a45fdb47f3a87a4c957
The24thDS <davidsima@windowslive.com>
2021-04-12 17:43:35 +0300

    Fixed images containing spaces not appearing

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,7 +1,7 @@
 const API_URL = process.env.API_URL || 'http://localhost:3001';
 
 export const getImagePath = (imageName) =>
-  `${API_URL}/assets/images/${imageName}`;
+  encodeURI(`${API_URL}/assets/images/${imageName}`);
 
 export const getModelFilePath = (modelName) =>
   `${API_URL}/assets/models/${modelName}`;
