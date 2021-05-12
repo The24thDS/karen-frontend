8e66db42ebb54cc813589b7f6d2300fe1eba9799
The24thDS <davidsima@windowslive.com>
2021-05-12 14:59:52 +0300

    Changed assets functions according to backend

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,14 +1,10 @@
 const API_URL = process.env.API_URL || 'http://localhost:3001';
 
 export const getImagePath = (modelSlug, username, imageName) =>
-  encodeURI(
-    `${API_URL}/assets/images/${imageName}?modelAuthor=${username}&modelSlug=${modelSlug}`
-  );
+  encodeURI(`${API_URL}/assets/images/${username}/${modelSlug}/${imageName}`);
 
 export const getModelFilePath = (modelSlug, username, fileName) =>
-  encodeURI(
-    `${API_URL}/assets/models/${fileName}?modelAuthor=${username}&modelSlug=${modelSlug}`
-  );
+  encodeURI(`${API_URL}/assets/models/${username}/${modelSlug}/${fileName}`);
 
 export const getGltfFilePath = (modelSlug, username, fileName) =>
-  `${API_URL}/assets/gltf/${username}/${modelSlug}/${fileName}`;
+  encodeURI(`${API_URL}/assets/gltf/${username}/${modelSlug}/${fileName}`);
