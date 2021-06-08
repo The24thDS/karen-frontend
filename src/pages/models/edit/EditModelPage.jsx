c139945c3350b9d417e4bcc965602307da44e651
The24thDS <davidsima@windowslive.com>
2021-06-08 19:25:00 +0300

    Fixed not being able to access edit model page

--- src/pages/models/edit/EditModelPage.jsx
+++ src/pages/models/edit/EditModelPage.jsx
@@ -6,23 +6,28 @@ import { tw } from 'twind';
 import { updateModel } from 'api/models.api';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from 'state/selectors/users.selectors';
+import WithLoading from 'components/with-loading/WithLoading';
 
 const EditModelPage = () => {
   const { slug } = useParams();
   const model = useModel(slug);
   const loggedInUser = useSelector(selectCurrentUser);
-  return loggedInUser?.username === model.user?.username ? (
-    <div className="px-6 mx-20 md:mt-12">
-      <h1 className={tw`text(center black xl) font-semibold`}>
-        Editing model <em>{slug}</em>
-      </h1>
-      <ModelForm
-        initialModel={model}
-        onFormSubmit={(data) => updateModel(data, slug)}
-      />
-    </div>
-  ) : (
-    <Redirect to={'/'} />
+  return (
+    <WithLoading condition={model.user.username}>
+      {loggedInUser?.username === model.user?.username ? (
+        <div className="px-6 mx-20 md:mt-12">
+          <h1 className={tw`text(center black xl) font-semibold`}>
+            Editing model <em>{slug}</em>
+          </h1>
+          <ModelForm
+            initialModel={model}
+            onFormSubmit={(data) => updateModel(data, slug)}
+          />
+        </div>
+      ) : (
+        <Redirect to={'/'} />
+      )}
+    </WithLoading>
   );
 };
 
